7a4ce726458e9c71681b31e61489b1a1412cec74
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-29 17:19:25 +0200

    ADD selector page component, service, routing

---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [],
+})
+export class SelectorPageComponent implements OnInit {
+  miFormulario: FormGroup = this.formBuilder.group({
+    region: ['', [Validators.required]],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  guardar() {
+    console.log(this.miFormulario.value);
+  }
+}
